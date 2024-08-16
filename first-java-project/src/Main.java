517fe77e971bec027f2ab9ca61e5e64acd0f8273
Ibrahim Fradj <Fradj.ibrahim@gmail.com>
2024-08-15 21:47:20 -0400

    finis bases de Java

--- first-java-project/src/Main.java
+++ first-java-project/src/Main.java
@@ -2,8 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello world");
+
+       int [] nombres = {3,4,5,6,76};
+
+       int cible = 10;
+
+       int indice = -1;
+
+       for (int i = 0; i < nombres.length; i++) {
+           if (nombres[i] == cible) {
+               indice = i;
+               break;
+           }
+       }
+
+       if(indice == -1){
+           System.out.println("Le nombre n'existe pas");
+       } else {
+           System.out.println("on a trouve le nombre a l'indice" + indice);
+       }
     }
 }
